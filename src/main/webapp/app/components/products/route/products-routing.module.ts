6e3eb974c7cfe08c852fb23ca8378c8764d17b1d
HAMICI Ryadh <hamiciryadh@gmail.com>
2021-11-11 14:13:36 +0100

    -L'accès à la liste des produits ne nécessite désormais plus que l'utilisateur soit authentifié. -Modification de l'icone de "Produits" au niveau du menu.

--- src/main/webapp/app/components/products/route/products-routing.module.ts
+++ src/main/webapp/app/components/products/route/products-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProductsComponent } from '../list/products.component';
-import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
 
 const productsRoute: Routes = [
   {
     path: '',
     component: ProductsComponent,
-    canActivate: [UserRouteAccessService],
   },
 ];
 
