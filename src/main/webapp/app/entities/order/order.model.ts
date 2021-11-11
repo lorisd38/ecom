58020bd4cfcde24ad8aa70d815354dbb37a9985a
HAMICI Ryadh <hamiciryadh@gmail.com>
2021-11-11 17:38:17 +0100

    -Modifications liées à l'entité "Order": Ajout d'un nouveau champ "receptionDate".

--- src/main/webapp/app/entities/order/order.model.ts
+++ src/main/webapp/app/entities/order/order.model.ts
@@ -4,6 +4,7 @@ import { IProductOrder } from 'app/entities/product-order/product-order.model';
 export interface IOrder {
   id?: number;
   paymentDate?: dayjs.Dayjs;
+  receptionDate?: dayjs.Dayjs;
   promoCode?: string | null;
   totalPrice?: number;
   lines?: IProductOrder[] | null;
@@ -13,6 +14,7 @@ export class Order implements IOrder {
   constructor(
     public id?: number,
     public paymentDate?: dayjs.Dayjs,
+    public receptionDate?: dayjs.Dayjs,
     public promoCode?: string | null,
     public totalPrice?: number,
     public lines?: IProductOrder[] | null
