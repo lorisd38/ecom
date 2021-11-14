d8fa650ded0ef86af846f87bde85af4f64ce8111
m2gi-davilori <pixel38320@gmail.com>
2021-11-14 19:31:01 +0100

    Ajout du compteur \"- q +\" avec recherche de la quantite du produit dans le panier si il est present dans ce dernier. Preparation des fonctions increase et decrease.

--- src/main/webapp/app/components/products/list/products.component.ts
+++ src/main/webapp/app/components/products/list/products.component.ts
@@ -5,6 +5,8 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { IProduct } from 'app/entities/product/product.model';
 import { ProductService } from 'app/entities/product/service/product.service';
 import { ProductToCartService } from '../service/product-to-cart.service';
+import { ICart } from '../../../entities/cart/cart.model';
+import { CartService } from '../../cart/service/cart.service';
 
 @Component({
   selector: 'jhi-products',
@@ -12,26 +14,77 @@ import { ProductToCartService } from '../service/product-to-cart.service';
 })
 export class ProductsComponent implements OnInit {
   products?: IProduct[];
+  cart?: ICart | null;
   isLoading = false;
+  productsPresent: number[] = [];
 
   constructor(
     protected productService: ProductService,
     protected modalService: NgbModal,
-    protected productToCartService: ProductToCartService
+    protected productToCartService: ProductToCartService,
+    public cartService: CartService
   ) {}
 
-  loadAll(): void {
+  loadProduct(): void {
     this.isLoading = true;
 
     this.productService.query().subscribe(
       (res: HttpResponse<IProduct[]>) => {
-        this.isLoading = false;
         this.products = res.body ?? [];
       },
       () => {
         this.isLoading = false;
       }
     );
+  }
+
+  loadCart(): void {
+    this.cartService.queryOneCart().subscribe(
+      (res: HttpResponse<ICart>) => {
+        this.isLoading = false;
+        this.cart = res.body ?? null;
+        if (this.cart?.lines != null) {
+          for (const lineProduct of this.cart.lines) {
+            if (lineProduct.product?.id != null) {
+              this.productsPresent.push(lineProduct.product.id);
+            }
+          }
+        }
+      },
+      () => {
+        this.isLoading = false;
+      }
+    );
+  }
+
+  loadAll(): void {
+    this.loadProduct();
+    this.loadCart();
+  }
+
+  isPresent(itemId?: number): boolean {
+    return itemId != null ? this.productsPresent.includes(itemId) : false;
+  }
+
+  decreaseProduct(item: IProduct): void {
+    return;
+  }
+
+  increaseProduct(item: IProduct): void {
+    return;
+  }
+
+  quantityProduct(item: IProduct): string {
+    if (this.cart?.lines != null) {
+      for (const lineProduct of this.cart.lines) {
+        if (lineProduct.product?.id === item.id) {
+          if (lineProduct.quantity != null) {
+            return lineProduct.quantity.toString();
+          }
+        }
+      }
+    }
+    return '?';
   }
 
   ngOnInit(): void {
